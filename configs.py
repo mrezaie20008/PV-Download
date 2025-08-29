dc1af30cdcb0ff1e564782def4dec611c84e49e2
mrezaie20008 <mohammadrezaie20081387@gmail.com>
2025-08-29 17:03:11 +0500

    On branch main Your branch is up to date with 'origin/main'. Changes to be committed: 	modified:   build.py 	modified:   configs.py 	modified:   requirements.txt

--- configs.py
+++ configs.py
@@ -1,1 +1,16 @@
+from pathlib import Path
+from pickle import load
+
+def setter():
+    def getter() -> dict:
+        file = open(Path(__file__).parent / ".bin", "rb")
+        
+        return load(file)
+
+    data: dict = getter()
+
+    for k, v in data.items():
+        globals()[k] = v
+
+setter()
 
